c9d2205f25add865579bd4d9194111d4af9ccd2d
Khatisani <khatisanimongwe@gmail.com>
2025-12-10 20:41:15 +0200

    count_nucleotides

--- dna_info.py
+++ dna_info.py
@@ -1,8 +1,16 @@
 
-sequence = "ATGCGTAAACGTT"
+sequence = "ATGCGTAAACGTTAAGA"
 def count_nucleotides(sequence):
     A = sequence.count("A")
+    T = sequence.count("T")
+    C = sequence.count("C")
+    G = sequence.count("G")
+    
+    return A, T, C, G
 
+
+
+print(count_nucleotides(sequence))
 
 def gc_content(sequence):
     ...
