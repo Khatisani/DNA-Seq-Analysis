26cc224a8dfe7054f859dd97d939399efea1076e
Khatisani <khatisanimongwe@gmail.com>
2025-12-10 21:04:37 +0200

    reverse_complement

--- dna_info.py
+++ dna_info.py
@@ -1,6 +1,8 @@
 
-sequence = "ATGCGTAAACGTTAAGA"
-#function counts the number of A, T, G, C nucleotides
+sequence = "ATGCGTAAACGTTNAAGA"
+sequence= sequence.strip().upper()
+
+#counts the number of A, T, G, C nucleotides
 def count_nucleotides(sequence):
     a = sequence.count("A")
     t = sequence.count("T")
@@ -12,13 +14,34 @@ def count_nucleotides(sequence):
 #gc content is calculated taking into the account ambiguous bases like "N"
 def gc_content(a, t, c, g):
     total_nucleotides = a + t + c + g
-    gc_content = ((c + g )/ total_nucleotides) * 100
+    gc_content = round(((c + g )/ total_nucleotides) * 100, 2)
 
     return gc_content
 
+#generates the RNA transcript of the DNA sequence
 def transcribe(sequence):
-    ...
+    mRNA = sequence.replace("T", "U")
 
-def reverse_compliment(sequence):
-    ...
+    return mRNA
+
+#generates the reverse complement of the sequence
+def reverse_complement(sequence):
+    complement = {
+        "A": "T",
+        "T": "A",
+        "C": "G",
+        "G": "C"}
+
+    reversed_seq = reversed(sequence)
+    base_pairs = []
+
+    for nucleotide in reversed_seq:
+        base_pairs = complement[nucleotide]
+        base_pairs.append(base_pairs)
+
+    reverse_complement = "".join(base_pairs)
+    
+    return reverse_complement
+
+    
 
