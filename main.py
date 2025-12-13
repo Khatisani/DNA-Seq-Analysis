a0904db00aa7c4722ed98bb48117fd05ceb6f344
Khatisani <khatisanimongwe@gmail.com>
2025-12-13 22:21:48 +0200

    parse fasta

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 import csv
 
-def parse_fasta(file):
-    ...
+input_file = "example.fasta"
+def parse_fasta(input_file):
+
+    sequences = {}
+    id = None
+    lines = []
+
+    with open(input_file, "r") as file:
+        for line in file:
+            line = line.strip()  
+            if not line:
+                    continue 
+            if line.startswith(">"):
+                if id:
+                    sequences[id] = "".join(lines).upper()
+                       
+                id = line[1:].split()[0]
+                lines = []
+            else:
+                lines.append(line)
+
+           
+        if id:
+            sequences[id] = "".join(lines).upper()
+
+    return sequences
+
 
 def write_to_csv():
     ...
