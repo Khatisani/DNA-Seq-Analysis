045d308ac6712671c3cf970fadab47c764ef7485
Khatisani <khatisanimongwe@gmail.com>
2025-12-13 21:58:03 +0200

    motif search

--- motif_search.py
+++ motif_search.py
@@ -1,4 +1,14 @@
 
 def motif_search(sequence,  motif):
-    ...
 
+    sequence = sequence.upper()
+    motif = motif.upper()
+    
+    motif_positions = []
+    motif_length = len(motif)
+    
+    for i in range(len(sequence) - motif_length + 1):
+        if sequence[i:i+motif_length] == motif:
+            motif_positions.append(i + 1)
+    
+    return motif_positions
