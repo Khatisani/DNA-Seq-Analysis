14f113260378246b558c9b134d5017fbf2c1402c
Khatisani <khatisanimongwe@gmail.com>
2025-12-13 22:06:43 +0200

    move motif search

--- dna_info.py
+++ dna_info.py
@@ -44,5 +44,22 @@ def reverse_complement(sequence):
 
     return reverse_complement
 
+#finds the position of the motif in a DNA sequence
+def motif_search(sequence,  motif):
+
+    sequence = sequence.upper()
+    motif = motif.upper()
+    
+    positions = []
+        
+    i = 0
+    while True:
+        i = sequence.find(motif, i)
+        if i == -1:
+            break
+        positions.append(i + 1)
+        i += 1 
+        
+    return positions
     
 
