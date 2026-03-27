6092c1859bc0ca339de84ff58346011007db8136
Khatisani <khatisanimongwe@gmail.com>
2026-03-27 18:52:55 +0200

    motif_search(), replaced manual calculation with nt_search from Biopython

--- dna_info.py
+++ dna_info.py
@@ -1,5 +1,5 @@
 from Bio.Seq import Seq
-from Bio.SeqUtils import gc_fraction 
+from Bio.SeqUtils import gc_fraction, nt_search
 
 #counts the number and percentage of A, T, G, C nucleotides
 def count_nucleotides(sequence):
@@ -11,35 +11,26 @@ def count_nucleotides(sequence):
 
     return nucleotide_counts, nucleotide_percentage
 
+
 #gc content is calculated taking into the account ambiguous bases like "N"
 def gc_content(sequence):
     gc_percentage = gc_fraction(sequence) *100
     return round(gc_percentage, 2)
 
+
 #generates the RNA transcript of the DNA sequence
 def transcribe(sequence):
     return str(Seq(sequence).transcribe())
+
 
 #generates the reverse complement of the sequence
 def reverse_complement(sequence):
     return str(Seq(sequence).reverse_complement())
 
+
 #finds the position of the motif in a DNA sequence
 def motif_search(sequence,  motif):
-
-    sequence = sequence.upper()
-    motif = motif.upper()
-    
-    positions = []
-        
-    i = 0
-    while True:
-        i = sequence.find(motif, i)
-        if i == -1:
-            break
-        positions.append(i + 1)
-        i += 1 
-        
-    return positions
-    
+    output = nt_search(str(sequence). upper(), motif.upper())
+    motif_positions = [position + 1 for position in output[1:]]
+    return motif_positions
 
